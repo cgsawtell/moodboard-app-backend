89538ff79729dcdb833d90a490ebbb256fe82eff
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-03-14 15:58:09 +1300

    fixed issue where user/boards route wasn't being hit

--- routes/private/user.js
+++ routes/private/user.js
@@ -2,8 +2,8 @@ const Router = require('koa-router')
 const user = require('../../controllers/user')
 const router = new Router()
 
+router.get('/user/boards', user.boards)
 router.get('/user/:id', user.findFromId)
 router.put('/user', user.update)
-router.get('/user/boards', user.boards)
 
 module.exports = router.middleware()
